1c9a07555b708830faa5d5749878962c039b30c0
cyelmini <cyelmini@itba.edu.ar>
2024-12-06 10:25:14 -0300

    se arreglo problema cuadrados
    Co-Authored-By: Francisco Trivelloni <166187838+Ftrivelloni@users.noreply.github.com>
    Co-Authored-By: Mateo Lopez Badias <152566466+mlopezbadias@users.noreply.github.com>

--- src/frontend/Draw/DrawSquare.java
+++ src/frontend/Draw/DrawSquare.java
@@ -2,6 +2,7 @@ package frontend.Draw;
 
 import backend.model.Figure;
 import backend.model.Point;
+import backend.model.Rectangle;
 import backend.model.Square;
 import frontend.Layer;
 import frontend.ShadowType;
@@ -17,6 +18,57 @@ public class DrawSquare extends DrawRectangle {
         figure = new Square(topLeft, size);
         square = (Square)getFigure();
 
+    }
+
+    @Override
+    public void draw(Figure selectedFigure, Color lineColor){
+        drawShadow();
+
+        drawBeveled();
+
+        setGradientLinear();
+
+        getGc().fillRect(square.getTopLeft().getX(), square.getTopLeft().getY(),
+                square.width(), square.height());
+
+        if(selected(selectedFigure)) {
+            gc.setStroke(Color.RED);
+        } else {
+            gc.setStroke(lineColor);
+        }
+
+        getGc().strokeRect(square.getTopLeft().getX(), square.getTopLeft().getY(),
+                square.width(), square.height());
+    }
+
+    @Override
+    public void turnRight(){
+        square.turnRight();
+    }
+
+    @Override
+    public void turnHorizontal(){
+        square.turnHorizontal();
+    }
+
+    @Override
+    public void turnVertical(){
+        square.turnVertical();
+    }
+
+    @Override
+    public DrawRectangle divide(){
+        Rectangle temp = square.divideFigure();
+        return new DrawRectangle(temp.getTopLeft(), temp.getBottomRight(), getFillColor(), getGradientColor(), gc, getShadowType(), isBeveled(), getLayer());
+    }
+
+    @Override
+    public DrawRectangle duplicate(double offset){
+        Point duplicatedTopLeft = new Point(square.getTopLeft().getX(), square.getTopLeft().getY());
+        Point duplicatedBottomRight = new Point(square.getBottomRight().getX(), square.getBottomRight().getY());
+        duplicatedTopLeft.move(offset, offset);
+        duplicatedBottomRight.move(offset, offset);
+        return new DrawRectangle(duplicatedTopLeft, duplicatedBottomRight, getFillColor(), getGradientColor(), gc, getShadowType(), isBeveled(), getLayer());
     }
 
 }
